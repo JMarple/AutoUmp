a8f85f7dbd6301e2e6e8751fd806aadf65f9d9c0
Timothy Adams <tbadams45@gmail.com>
2017-03-05 14:38:16 -0500

    add foldersaving functionality to FullImageViewer - implement (slow) denoise

--- AutoUmp/.build/src/AutoUmp.xc.d
+++ AutoUmp/.build/src/AutoUmp.xc.d
@@ -1,5 +1,5 @@
-.build/src//AutoUmp.xc.d ./.build/src//AutoUmp.xc.o ./.build/src//AutoUmp.xc.pca.xml: ./src/AutoUmp.xc \
-  ../../AutoUmp/AutoUmp/src/cameras/ov07740.h \
-  ../../AutoUmp/AutoUmp/src/io/io.h \
-  ../../AutoUmp/AutoUmp/src/cameras/algs.h
+.build/src/AutoUmp.xc.d .build/src/AutoUmp.xc.o .build/src/AutoUmp.xc.pca.xml: ./src/AutoUmp.xc \
+  ../../autoump/AutoUmp/src/cameras/ov07740.h \
+  ../../autoump/AutoUmp/src/io/io.h \
+  ../../autoump/AutoUmp/src/cameras/algs.h
 
--- FullImageViewer/src/main.cpp
+++ FullImageViewer/src/main.cpp
@@ -4,6 +4,10 @@ extern "C"
 }
 #include <cstdint>
 #include <iostream>
+#include <sys/stat.h> // to determine folder
+#include <string>
+#include <sstream>
+#include <iostream>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -11,7 +15,9 @@ extern "C"
 
 using namespace cv;
 
-int main()
+// usage: ./auview absolute_path_to_folder
+// the primary folder will hold a bunch of sub folders
+int main(int argc, char* argv[])
 {
     char mode[] = "8N1";
     const int COM_PORT = 0;
@@ -21,10 +27,33 @@ int main()
         return 0;
     }
 
+	std::string folderName(argv[1]);
+/*
+	struct stat sb; 
+	std::ostringstream pn; 
+	int folderCount = 0;
+	pn << argv[1] << "/" << folderCount;
+	std::string pathname = pn.str();
+*/
+/* 
+	if(stat(pathname.c_str(), &sb) == 0 && S_ISDIR(sb.st_mode))
+	{
+		// pathname is a directory
+		folderCount++;
+		pn << argv[1] << "/" << folderCount;
+		pathname = pn.str();
+	}
+	else
+	{
+		// pathname is not a directory
+		mkdir(pathname.c_str(), 0777);
+	}
+*/
     Mat M(240, 320, CV_8UC1, Scalar(0, 0, 0));
 
     const int32_t size = 320*240;
     uint8_t currentImage[size];
+	int32_t currentImageNum = 0;
     int32_t index = 0;
 
     while (1==1)
@@ -39,8 +68,12 @@ int main()
         // Show Image
         imshow("a", M);
         waitKey(1);
+		std::ostringstream fileName;
+		fileName << folderName << "/" << currentImageNum << ".png";
+		imwrite(fileName.str(), M);
         printf("Frame recieved!\n");
         index = 0;
+		currentImageNum++;
     }
 
     RS232_CloseComport(COM_PORT);
