5cd8e3e5c6e127c0079c7303a1112dc9c45890b0
Timothy Adams <tbadams45@gmail.com>
2017-03-07 23:58:32 -0500

    bitImageViewer has capacity to show color pictures, store images in given folder, and unpack objects

--- BitImageViewer/src/main.cpp
+++ BitImageViewer/src/main.cpp
@@ -4,6 +4,9 @@ extern "C"
 }
 #include <cstdint>
 #include <iostream>
+#include <cstdint>
+#include <algorithm>
+#include <string>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -11,30 +14,247 @@ extern "C"
 
 using namespace cv;
 
-int main()
+#define IMG_WIDTH 320
+#define IMG_HEIGHT 240
+#define EMPTY_OBJECT_ID 65535
+
+
+#define RED 0
+#define GREEN 1
+#define BLUE 2
+
+// makes a red cross hair, with the center at the given x, y position.
+void makeCrosshairs(Mat* colorImage, uint16_t x, uint16_t y, uint16_t color)
 {
-    char mode[] = "8N1";
-    const int COM_PORT = 0;
-    if (RS232_OpenComport(COM_PORT, 921600, mode))
+    // cross hair is going to have tails that are 5 pixels long and 1 pixel wide.
+    Vec3b bgr;
+    
+    if(color == RED)
+    {
+        bgr.val[0] = 0; // blue
+        bgr.val[1] = 0; // green
+        bgr.val[2] = 255; // red
+    }
+    else if(color == GREEN)
+    {
+        bgr.val[0] = 0; // blue
+        bgr.val[1] = 255; // green
+        bgr.val[2] = 0; // red 
+    }
+    else if(color == BLUE)
+    {
+        bgr.val[0] = 255; // blue
+        bgr.val[1] = 0; // green
+        bgr.val[2] = 0; // red     
+    }
+
+
+    int8_t crosshairsLength = 5;
+
+    // go along columns
+	int tmpXLower = max(x-crosshairsLength, 0);
+	int tmpXUpper = min(x+crosshairsLength, IMG_WIDTH);
+    for(int i = tmpXLower; i <= tmpXUpper; i++)
+    {
+        colorImage->at<Vec3b>(Point(i,y)) = bgr;
+    }
+
+    // go along rows. Some redudancy, but w/e...
+	int tmpYLower = max(y-crosshairsLength, 0);
+	int tmpYUpper = min(y+crosshairsLength, IMG_HEIGHT);
+    for(int j = tmpYLower; j <= tmpYUpper; j++)
+    {
+        colorImage->at<Vec3b>(Point(x,j)) = bgr;
+    }
+}
+
+
+void makeLine(Mat* colorImage, uint16_t rowOrColumn, uint16_t color, uint16_t isVertical)
+{
+    Vec3b bgr;
+    
+    if(color == RED)
+    {
+        bgr.val[0] = 0; // blue
+        bgr.val[1] = 0; // green
+        bgr.val[2] = 255; // red
+    }
+    else if(color == GREEN)
+    {
+        bgr.val[0] = 0; // blue
+        bgr.val[1] = 255; // green
+        bgr.val[2] = 0; // red 
+    }
+    else if(color == BLUE)
+    {
+        bgr.val[0] = 255; // blue
+        bgr.val[1] = 0; // green
+        bgr.val[2] = 0; // red     
+    }
+
+    if(isVertical)
+    {
+        for(int i = 0; i < IMG_HEIGHT; i++)
+        {   
+            colorImage->at<Vec3b>(Point(rowOrColumn, i)) = bgr;
+        }
+    }
+    else
+    {
+        for(int i = 0; i < IMG_WIDTH; i++)
+        {   
+            colorImage->at<Vec3b>(Point(i, rowOrColumn)) = bgr;
+        }
+    }
+}
+
+struct Object
+{
+    uint8_t  isBall; // -1 = not checked, 0 = no, 1 = yes
+    uint16_t id; // id representing object
+    uint16_t minX, maxX, minY, maxY; // lower/uppper bounds of object
+    uint16_t centX, centY;
+    uint16_t distanceFromCenter;
+};
+
+void unpackCenters(
+    struct Object* objArray,
+    uint8_t* buffer,
+    uint16_t bufferLength)
+{
+    for(int i = 0; i < bufferLength; i+=4)
+    {
+        uint8_t xLower = buffer[i];
+        uint8_t xUpper = buffer[i+1];
+        uint8_t yLower = buffer[i+2];
+        uint8_t yUpper = buffer[i+3];
+
+		uint16_t centX = (xUpper << 8) | xLower;
+		uint16_t centY = (yUpper << 8) | yLower;
+
+		if(centX == 65535) // that's our cue -- we've hit our last object
+		{
+			break;
+		}
+
+        objArray[i/4].centX = centX;
+        objArray[i/4].centY = centY;
+    }
+}
+
+
+void packCenters(
+    struct Object* objArray,
+    uint8_t* buffer,
+    int32_t numObjects)
+{
+    for(int i = 0; i < numObjects; i++)
+    {
+        uint8_t xLower = objArray[i].centX & 0xFF;
+        uint8_t xUpper = objArray[i].centX >> 8;
+        uint8_t yLower = objArray[i].centY & 0xFF;
+        uint8_t yUpper = objArray[i].centY >> 8;
+
+        buffer[i*4] = xLower;
+        buffer[i*4 + 1] = xUpper;
+        buffer[i*4 + 2] = yLower;
+        buffer[i*4 + 3] = yUpper;
+    }
+    if(numObjects < 250) // send a code: we're done with our objects
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            buffer[numObjects*4 + i] = 0xFF;
+        }
+    }
+}
+
+void initObjectArray(struct Object* objArray, uint16_t length)
+{
+    for (int i = 0; i < length; i ++)
+    {
+        objArray[i].id = EMPTY_OBJECT_ID;
+        objArray[i].isBall = -1;
+        objArray[i].minX = IMG_WIDTH;
+        objArray[i].maxX = 0;
+        objArray[i].minY = IMG_HEIGHT;
+        objArray[i].maxY = 0;
+        objArray[i].centX = 0;
+        objArray[i].centY = 0;
+        objArray[i].distanceFromCenter = 0;
+    }
+}
+
+void printCenters(struct Object* objArray, uint16_t length)
+{
+    uint16_t i = 0;
+    while((i < length) && (objArray[i].centX != 65535))
+    {
+        printf("centX: %i; centY: %i \n",
+            objArray[i].centX,
+            objArray[i].centY);
+        i++;
+    }
+    printf("\n");
+}
+
+
+int main(int argc, char** argv)
+{
+	char mode[] = "8N1";
+   	const int COM_PORT = 0;
+   /* if (RS232_OpenComport(COM_PORT, 921600, mode))
     {
         std::cout << "Could not find com port" << std::endl;
         return 0;
-    }
+    }*/
+	
+	// these are only used if we are given a foldername to store files
+	std::string folderName;
+	int32_t currentImageNumber = 0;
+	if(argc == 2)
+	{
+		folderName = std::string(argv[1]);
+	}
 
+	// general init
     Mat M(240, 320, CV_8UC1, Scalar(0, 0, 0));
-
+	Mat M_color;
     const int32_t size = 40*240;
     uint8_t currentImage[size];
-    int32_t index = 0;
+    int32_t indexPic = 0;
+	int32_t indexObjects = 0;
+	const int32_t sizeObjects = 250*4; // 250 objects * 4 bytes to represent the center
+	uint8_t objectBuffer[sizeObjects];
+	struct Object objArray[250];
+	initObjectArray(objArray, 250);
+
+	// make fake data
+	struct Object fakeObjArray[250];
+	initObjectArray(fakeObjArray, 250);
+	for(int i = 0; i < 50; i++)
+	{
+		fakeObjArray[i].centX = i*5; 
+		fakeObjArray[i].centY = i*3;
+	}
+
+	packCenters(fakeObjArray, objectBuffer, 50); 
 
     while (1==1)
     {
-        // Get frame from UART
-        while (index < size)
+     /*   // Get frame from UART
+        while (indexPic < size)
         {
-            int len = RS232_PollComport(COM_PORT, &(currentImage[index]), size - index);
-            index += len;
+            int len = RS232_PollComport(COM_PORT, &(currentImage[indexPic]), size - indexPic);
+            indexPic += len;
         }
+		
+		// get object array
+		while (indexObjects < sizeObjects)
+		{
+			int len = RS232_PollComport(COM_PORT, &(objectBuffer[sizeObjects]), sizeObjects - indexObjects);
+			indexObjects += len;
+		}
 
         for (int idx = 0; idx < size; idx++)
         {
@@ -45,16 +265,41 @@ int main()
                M.data[idx*8 + i] = ((data >> i) & 0b1)*255;
 //				M.data[idx*8 + (7-i)] = ((data >> i) & 0b1)*255;
             }
-        }
+        }*/
+		
+		unpackCenters(objArray, objectBuffer, sizeObjects); 
 
-        // Show Image
-        imshow("a", M);
+        cvtColor(M, M_color, cv::COLOR_GRAY2BGR);
+		makeLine(&M_color, 160, GREEN, 1);
+
+		// show fake data
+		for(int i = 0; i < 50; i++)
+		{
+			makeCrosshairs(&M_color, objArray[i].centX, objArray[i].centY, RED);
+		} 
+		imshow("a", M_color);
         waitKey(1);
-        printf("Frame recieved!\n");
-        index = 0;
-    }
 
-    RS232_CloseComport(COM_PORT);
+		// only do anything here if we're given a folder name to store files
+		if(argc == 2)
+		{
+			std::ostringstream fileName;
+			fileName << folderName << "/" << currentImageNumber << ".png";
+			imwrite(fileName.str(), M_color);
+			printf("Stored in %s\n", fileName.str().c_str());
+			currentImageNumber++;
+			sleep(1);
+		}
+		else
+		{
+        	printf("Frame recieved!\n");
+		}
+        
+		indexPic = 0;
+		indexObjects = 0;
+	}
+
+ //   RS232_CloseComport(COM_PORT);
     std::cout << "Done!" << std::endl;
 
     return 0;
