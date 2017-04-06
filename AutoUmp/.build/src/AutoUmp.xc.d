50763bfe725a3870fc7a8b8233baf4bd278dc7ca
Timothy Adams <tbadams45@gmail.com>
2017-04-06 17:57:00 -0400

    prep for push

--- AutoUmp/.build/src/AutoUmp.xc.d
+++
@@ -1,7 +0,0 @@
-.build/src//AutoUmp.xc.d ./.build/src//AutoUmp.xc.o ./.build/src//AutoUmp.xc.pca.xml: ./src/AutoUmp.xc \
-  ../../AutoUmp/AutoUmp/src/cameras/ov07740.h \
-  ../../AutoUmp/AutoUmp/src/cameras/algs.h \
-  ../../AutoUmp/AutoUmp/src/cameras/detect_objects.h \
-  ../../AutoUmp/AutoUmp/src/cameras/queue.h \
-  ../../AutoUmp/AutoUmp/src/io/io.h
-
--- AutoUmp/.build/src/io/io.xc.d
+++
@@ -1,2 +0,0 @@
-.build/src/io//io.xc.d ./.build/src/io//io.xc.o ./.build/src/io//io.xc.pca.xml: ./src/io/io.xc ../../AutoUmp/lib_locks/api/hwlock.h
-
